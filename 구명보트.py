bc9a31a91adf88fc68d6a4cb30480e44570fd812
JuJiwon <36442946+codingstr@users.noreply.github.com>
2021-12-26 16:46:35 +0900

    remove 대신 index변수를 사용
    성공

--- 구명보트.py
+++ 구명보트.py
@@ -4,17 +4,20 @@ def solution(people, limit):
     answer = 0
 
     people.sort(reverse = True)
+    largestN = 0
+    smallestN = len(people)-1
 
-    while len(people) > 0 :
-        i = people[0]
+    while largestN < smallestN :
+        i = people[largestN]
         if i <= limit/2 :
-            answer += len(people)//2 + len(people)%2
+            answer += (smallestN-largestN+1)//2 + (smallestN-largestN+1)%2
             break
         else :
-            j = people[-1]
-            people.remove(i)
-            if i+j <= limit : people.remove(j)
+            j = people[smallestN]
+            if i+j <= limit : smallestN -= 1
+            largestN += 1
             answer += 1
+    if largestN == smallestN : answer += 1
                 
     return answer
 
