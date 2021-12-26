9804166ac46d647a06e5e834ec04dd34d6825436
JuJiwon <36442946+codingstr@users.noreply.github.com>
2021-12-26 15:20:51 +0900

    2중 for, 실패
    시간초과

--- 구명보트.py
+++ 구명보트.py
@@ -3,6 +3,22 @@
 def solution(people, limit):
     answer = 0
 
-    
+    people.sort(reverse = True)
+    remaining = list(range(len(people)))
+    departed = list()
+
+    for i in range(len(people)) :
+        if i not in departed :
+            remaining.remove(i)
+            departed.append(i)
+            for j in remaining :
+                if people[i] + people[j] <= limit :
+                    remaining.remove(j)
+                    departed.append(j)
+                    break
+            answer += 1
 
     return answer
+
+print(str(solution([70, 50, 80, 50], 100))+', 3')
+print(str(solution([70, 80, 50], 100))+', 3')
