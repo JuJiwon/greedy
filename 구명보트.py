6d2bb8a104288b57d1c969aa5aa1f351a5afa487
JuJiwon <36442946+codingstr@users.noreply.github.com>
2021-12-26 15:57:59 +0900

    while문, 실패
    remaining, departed 삭제
    people로 처리
    시간초과

--- 구명보트.py
+++ 구명보트.py
@@ -4,20 +4,16 @@ def solution(people, limit):
     answer = 0
 
     people.sort(reverse = True)
-    remaining = list(range(len(people)))
-    departed = list()
-    
-    while len(remaining) > 1 :
-        i = remaining[0]
-        for j in remaining[1:] :
-            if people[remaining[0]] + people[j] <= limit :
-                remaining.remove(j)
-                departed.append(j)
+
+    while len(people) > 1 :
+        i = people[0]
+        for j in people[1:] :
+            if i + j <= limit :
+                people.remove(j)
                 break
-        remaining.remove(i)
-        departed.append(i)
+        people.remove(i)
         answer += 1
-    if len(remaining) == 1 : answer += 1
+    if len(people) == 1 : answer += 1
     return answer
 
 print(str(solution([70, 50, 80, 50], 100))+', 3')
