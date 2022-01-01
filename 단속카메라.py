2249491110456d5173682e16cd8321d794e7409c
JuJiwon <36442946+codingstr@users.noreply.github.com>
2022-01-01 19:01:12 +0900

    성공, 알고리즘 변경
    가장 먼저 빠져나가는 지점에 카메라를 설치, 반복

--- 단속카메라.py
+++ 단속카메라.py
@@ -1,27 +1,19 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42884
 
-def combineRoutes(routes) :
-    tempRoutes = routes[:]
-    i = 0
-    while i < len(tempRoutes) :
-        j = i+1
-        while j < len(tempRoutes) and tempRoutes[i][1] >= tempRoutes[j][0] :
-            start, end = [tempRoutes[i], tempRoutes[j]]
-            tempRoutes[i] = [end[0], start[1]] if start[1] < end[1] else [end[0], end[1]]
-            del tempRoutes[j]
-            print(tempRoutes)
-            j += 1
-        i += 1
-    if tempRoutes != routes : return combineRoutes(tempRoutes)
-    else :
-        return len(tempRoutes)
-
 def solution(routes):
 
     answer = 0
-    routes.sort(key = lambda x: x[0])
+    routes.sort(key = lambda x: x[1])
 
-    answer = combineRoutes(routes)
+    while len(routes) > 0 :
+        answer += 1
+        i = 1
+        while i < len(routes) :
+            route = routes[i]
+            if route[0] <= routes[0][1] : del routes[i]
+            else : i += 1
+        del routes[0]
+
     return answer
 
 print(str(solution([[-20,-15], [-14,-5], [-18,-13], [-5,-3]]))+', 2')
