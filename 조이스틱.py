b74e3046f7e0a14a94f721fa03f25dbd7c59486a
JuJiwon <36442946+codingstr@users.noreply.github.com>
2022-01-14 15:07:08 +0900

    리팩토링, 테케12 실패

--- 조이스틱.py
+++ 조이스틱.py
@@ -1,9 +1,5 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42860
 
-from collections import namedtuple
-from turtle import back
-
-
 def counter(target) :
     if target <= 26-target :
         return target
@@ -45,51 +41,46 @@ def solution(name) :
     # 인덱스 데이터로 변환
     for data in name :
         nameData.append(ord(data)-65)
-    
-    # nameData의 첫 데이터 처리
-    answer += counter(nameData[0])
 
     if 0 not in nameData :
-        del nameData[0]
         for data in nameData :
             answer += counter(data)+1
+        answer -= 1
     else :
-        # largestAlpha 찾음
         largestAlpha = searchLargestAlpha(nameData)
 
         # largestAlpha가 한쪽 끝에 붙어있다면 최소 이동 방향으로 처리함.
-        if largestAlpha[0] <= 1 :
+        if largestAlpha[0] == 0 :
             del nameData[:largestAlpha[1]+1]
-            for data in list(reversed(nameData)) :
+            for data in nameData :
                 answer += counter(data)+1
         elif largestAlpha[1] == len(nameData)-1 :
             del nameData[largestAlpha[0]:]
-            del nameData[0]
             for data in nameData :
                 answer += counter(data)+1
+            answer -= 1
         else :
-            frontDistance = ((largestAlpha[0]-1)*2)
+            frontDistance = (largestAlpha[0]-1)*2
             backDistance = (len(nameData)-largestAlpha[1]-1)*2-1
             # largestAlpha가 되돌아가는 횟수보다 작다면 처리함.
             if largestAlpha[1] <= frontDistance and len(nameData)-largestAlpha[0] <= backDistance :
-                del nameData[0]
                 for data in nameData :
                     answer += counter(data)+1
+                answer -= 1
             # largestAlpha가 되돌아가는 횟수보다 크므로 최소 이동 방향으로 처리한 뒤 되돌아가야 함.
             else :
-                del nameData[0]
                 if frontDistance <= backDistance :
-                    for data in nameData[:largestAlpha[0]-1] :
+                    for data in nameData[:largestAlpha[0]] :
                         answer += counter(data)
                     answer += frontDistance
-                    del nameData[:largestAlpha[1]]
-                    for data in list(reversed(nameData)) :
+                    del nameData[:largestAlpha[1]+1]
+                    for data in nameData :
                         answer += counter(data)+1
                 else :
-                    for data in nameData[largestAlpha[1]:] :
+                    for data in nameData[largestAlpha[1]+1:] :
                         answer += counter(data)
-                    answer += backDistance+1
-                    del nameData[largestAlpha[0]-1:]
+                    answer += backDistance
+                    del nameData[largestAlpha[0]:]
                     for data in nameData :
                         answer += counter(data)+1
                 
