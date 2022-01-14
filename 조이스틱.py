3f8e266a0d48015cb8933a239e0a1b7947eb1ff8
JuJiwon <36442946+codingstr@users.noreply.github.com>
2022-01-14 22:45:44 +0900

    갈아엎음, 테케 일부 실패
    완전탐색에서 greedy로 갈아엎음

--- 조이스틱.py
+++ 조이스틱.py
@@ -1,92 +1,42 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42860
 
-def counter(target) :
+def getMoves(target) :
     if target <= 26-target :
         return target
     else :
         return 26-target
 
-def searchLargestAlpha(nameData) :
-
-    # A 모두 찾고 인덱스 저장하기
-    alphaList = list()
-    wasAlpha = False
-    alphaStart = 0
-    alphaEnd = 0
-    for i in range(len(nameData)) :
-        if nameData[i] == 0 :
-            if not wasAlpha :
-                alphaStart = i
-                alphaEnd = i
-                wasAlpha = True
-            else :
-                alphaEnd = i
-        else :
-            if wasAlpha :
-                alphaList.append([alphaStart, alphaEnd])
-                wasAlpha = False
-    if alphaEnd == len(nameData)-1 :
-        alphaList.append([alphaStart, alphaEnd])
-
-    # 큰 A 찾기
-    largestAlpha = alphaList[0]
-    for alpha in alphaList : 
-        if largestAlpha[1]-largestAlpha[0]+1 < alpha[1]-alpha[0]+1 : largestAlpha = alpha
-    return largestAlpha
-
 def solution(name) :
+    print(name)
     answer = 0
-    nameData = list()
 
-    # 인덱스 데이터로 변환
+    # int 데이터로 변환
+    nameIdx = list()
     for data in name :
-        nameData.append(ord(data)-65)
+        nameIdx.append(ord(data)-65)
+    incorrectCharIdx = list()
 
-    if 0 not in nameData :
-        for data in nameData :
-            answer += counter(data)+1
-        answer -= 1
-    else :
-        largestAlpha = searchLargestAlpha(nameData)
+    for idx, inputChar in enumerate(nameIdx) :
+        if inputChar != 0 :
+            answer += getMoves(inputChar)
+            incorrectCharIdx.append(idx)
 
-        # largestAlpha가 한쪽 끝에 붙어있다면 최소 이동 방향으로 처리함.
-        if largestAlpha[0] == 0 :
-            del nameData[:largestAlpha[1]+1]
-            for data in nameData :
-                answer += counter(data)+1
-        elif largestAlpha[1] == len(nameData)-1 :
-            del nameData[largestAlpha[0]:]
-            for data in nameData :
-                answer += counter(data)+1
-            answer -= 1
-        else :
-            frontDistance = (largestAlpha[0]-1)*2
-            backDistance = (len(nameData)-largestAlpha[1]-1)*2-1
-            # largestAlpha가 되돌아가는 횟수보다 작다면 처리함.
-            if largestAlpha[1] <= frontDistance and len(nameData)-largestAlpha[0] <= backDistance :
-                for data in nameData :
-                    answer += counter(data)+1
-                answer -= 1
-            # largestAlpha가 되돌아가는 횟수보다 크므로 최소 이동 방향으로 처리한 뒤 되돌아가야 함.
-            else :
-                if frontDistance <= backDistance :
-                    for data in nameData[:largestAlpha[0]] :
-                        answer += counter(data)
-                    answer += frontDistance
-                    del nameData[:largestAlpha[1]+1]
-                    for data in nameData :
-                        answer += counter(data)+1
-                else :
-                    for data in nameData[largestAlpha[1]+1:] :
-                        answer += counter(data)
-                    answer += backDistance
-                    del nameData[largestAlpha[0]:]
-                    for data in nameData :
-                        answer += counter(data)+1
-                
+    if len(incorrectCharIdx) != 0 :
+        currentCursor = 0
+        while len(incorrectCharIdx) != 0 :
+            smallistDistance = float('inf')
+            nextIdx = 0
+            for charIdx in incorrectCharIdx :
+                distanceA, distanceB = abs(currentCursor-charIdx), abs(currentCursor)+abs(len(nameIdx)-charIdx-1)+1
+                if distanceA < smallistDistance or distanceB < smallistDistance :
+                    smallistDistance = distanceA if distanceA < distanceB else distanceB
+                    nextIdx = charIdx
+            answer += smallistDistance
+            currentCursor = nextIdx
+            incorrectCharIdx.remove(currentCursor)
 
     return answer
 
-print(str(solution("AKKAAAAAAAKKK"))+', 57')
-print(str(solution("KAAKKKKAAA"))+', 56')
+# print(str(solution("AKKAAAAAAAKKK"))+', 57')
+# print(str(solution("KAAKKKKAAA"))+', 56')
 print(str(solution("KKKAAK"))+', 44')
